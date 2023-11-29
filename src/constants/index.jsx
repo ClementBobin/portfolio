77f3f4ef421ed28af235ab9e913d6d3b07c5a541
ClementBobin <clementbobin@gmail.com>
2023-11-29 20:04:09 +0100

    fix previous review

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -31,7 +31,6 @@ import {
 import { useTranslation } from 'react-i18next';
 import Anchor from '../components/libStyleCompo/anchor';
 import { useState } from "react";
-import Ref from '../assets/referentielle_SIO.html';
 
 // Function to generate data for the portfolio
 const generateData = () => {
@@ -331,7 +330,7 @@ const generateData = () => {
       iconBg: "#45e862",
       date: t('experienceElement3Date'),
       points: [
-        <div><button className="underline" onClick={() => setShowWindowBts(true)}>bts SIO SLAM</button>-{'>'} <button onClick={() => setShowWindowRef(true)} className="underline">Tableau de compétence</button><Anchor title="Référentielle SIO" additionalInfo={<><iframe className="w-[85vw] h-[80vh]" src="src/assets/Tableau-Référentiel-SLAM-_2_.html"/><a href="src/assets/Tableau%20Référentiel%20SLAM%20(2).xlsx" download>Download Excel File</a></>} showWindow={showWindowRef} setShowWindow={setShowWindowRef}/><Anchor title="Bts SIO" additionalInfo={<><iframe className="w-[85vw] h-[80vh]" src={Ref}/></>} showWindow={showWindowBts} setShowWindow={setShowWindowBts}/></div>,
+        <div><button className="underline" onClick={() => setShowWindowBts(true)}>bts SIO SLAM</button>-{'>'} <button onClick={() => setShowWindowRef(true)} className="underline">Tableau de compétence</button><Anchor title="Référentielle SIO" additionalInfo={<><iframe className="w-[85vw] h-[80vh]" src="./Tableau-Référentiel-SLAM-_2_.html"/><a href="./Tableau%20Référentiel%20SLAM%20(2).xlsx" download>Download Excel File</a></>} showWindow={showWindowRef} setShowWindow={setShowWindowRef}/><Anchor title="Bts SIO" additionalInfo={<><iframe className="w-[85vw] h-[80vh]" src="./referentielle_SIO.html"/></>} showWindow={showWindowBts} setShowWindow={setShowWindowBts}/></div>,
       ]
     }
   ];
