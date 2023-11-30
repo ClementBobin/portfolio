18004c14fb611d36da5d8dd9bbf5781e773b7016
ClementBobin <clementbobin@gmail.com>
2023-11-30 21:55:33 +0100

    tech news

--- src/pages/TechNews.jsx
+++ src/pages/TechNews.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
 function TechNews() {
   return (
-    <div>TechNews</div>
+    <main className="sec">
+      <div className="blockCustom">
+        <div className="tile">
+          <h1 className="reminder">Veille technologique</h1>
+          <iframe className="wakeletEmbed min-h-[500px]" width="100%" src="https://embed.wakelet.com/wakes/GPWkoCiThpXFbF7mkRFSL/list?hide-cover=1&hide-description=1&hide-title=1" allow="autoplay"/>
+        </div>
+      </div>
+    </main>
   )
 }
 
