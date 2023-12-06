b985137b64d38b0b8bc0c935bbbc7aa0e2196ec1
ClementBobin <clementbobin@gmail.com>
2023-12-06 13:16:56 +0100

    fix lighthouse

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,179 +1,552 @@
-// Importing image assets
-import logo from "./c-logo.avif";
+"use strict";
 
-import backend from "./backend.png";
-import backendFallback from "./backend.png";
-import mobile from "./mobile.png";
-import mobileFallback from "./mobile.png";
-import web from "./web.avif";
-import webFallback from "./web.png";
-
-import menu from "./menu.svg";
-import close from "./close.svg";
-import send from "./send.svg";
-import command from "./command.png";
-import commandFallback from "./command.png";
-
-import github from "./github.png";
-import githubFallback from "./github.png";
-import linkedIn from "./linkedIn.png";
-import linkedInFallback from "./linkedIn.png";
-import codewars from "./codewars.svg";
-import stackoverflow from "./stackoverflow.png";
-import stackoverflowFallback from "./stackoverflow.png";
-import codepen from "./codepen.webp";
-import reddit from "./reddit.webp";
-
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-import sql from "./tech/sql.png";
-import php from "./tech/php.png";
-import python from "./tech/python.png";
-import vmware from "./tech/vmware.png";
-import virtualbox from "./tech/Virtualbox.png";
-import cssFallback from "./tech/css.png";
-import dockerFallback from "./tech/docker.png";
-import figmaFallback from "./tech/figma.png";
-import gitFallback from "./tech/git.png";
-import htmlFallback from "./tech/html.png";
-import javascriptFallback from "./tech/javascript.png";
-import mongodbFallback from "./tech/mongodb.png";
-import nodejsFallback from "./tech/nodejs.png";
-import reactjsFallback from "./tech/reactjs.png";
-import tailwindFallback from "./tech/tailwind.png";
-import typescriptFallback from "./tech/typescript.png";
-import sqlFallback from "./tech/sql.png";
-import phpFallback from "./tech/php.png";
-import pythonFallback from "./tech/python.png";
-import vmwareFallback from "./tech/vmware.png";
-import virtualboxFallback from "./tech/Virtualbox.png";
-import framer from "./tech/framer.png";
-import framerFallback from "./tech/framer.png";
-
-import project1 from "./portfolio.png";
-import project1Fallback from "./portfolio.png";
-import project2 from "./jobit.png";
-import project2Fallback from "./jobit.png";
-import project3 from "./tripguide.png";
-import project3Fallback from "./tripguide.png";
-
-import anonym from "./people/anonym.webp";
-
-import devTools from "./devTools.png";
-import hypervisor from "./hypervisor.png";
-import os from "./OS.png";
-import world from "./world.png";
-import learn from "./learn.png";
-import logique from "./logique.png";
-import competition from "./competition.png";
-import team from "./team.png";
-import devToolsFallback from "./devTools.png";
-import hypervisorFallback from "./hypervisor.png";
-import osFallback from "./OS.png";
-import worldFallback from "./world.png";
-import learnFallback from "./learn.png";
-import logiqueFallback from "./logique.png";
-import competitionFallback from "./competition.png";
-import teamFallback from "./team.png";
-
-import linux from "./linux.png";
-import windows from "./windows.png";
-import linuxFallback from "./linux.png";
-import windowsFallback from "./windows.png";
-
-// Exporting image assets
-export {
-  logo,
-  backend,
-  backendFallback,
-  mobile,
-  mobileFallback,
-  web,
-  webFallback,
-  github,
-  githubFallback,
-  menu,
-  close,
-  send,
-  css,
-  cssFallback,
-  docker,
-  dockerFallback,
-  figma,
-  figmaFallback,
-  git,
-  gitFallback,
-  html,
-  htmlFallback,
-  javascript,
-  javascriptFallback,
-  mongodb,
-  mongodbFallback,
-  nodejs,
-  nodejsFallback,
-  reactjs,
-  reactjsFallback,
-  tailwind,
-  tailwindFallback,
-  typescript,
-  typescriptFallback,
-  threejs,
-  framer,
-  framerFallback,
-  sql,
-  sqlFallback,
-  php,
-  phpFallback,
-  python,
-  pythonFallback,
-  vmware,
-  vmwareFallback,
-  virtualbox,
-  virtualboxFallback,
-  anonym,
-  command,
-  commandFallback,
-  linkedIn,
-  linkedInFallback,
-  codewars,
-  stackoverflow,
-  stackoverflowFallback,
-  codepen,
-  reddit,
-  devTools,
-  devToolsFallback,
-  hypervisor,
-  hypervisorFallback,
-  os,
-  osFallback,
-  world,
-  worldFallback,
-  learn,
-  learnFallback,
-  logique,
-  logiqueFallback,
-  competition,
-  competitionFallback,
-  team,
-  teamFallback,
-  linux,
-  linuxFallback,
-  windows,
-  windowsFallback,
-  project1,
-  project1Fallback,
-  project2,
-  project2Fallback,
-  project3,
-  project3Fallback,
-};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "anonym", {
+  enumerable: true,
+  get: function () {
+    return _anonym.default;
+  }
+});
+Object.defineProperty(exports, "backend", {
+  enumerable: true,
+  get: function () {
+    return _backend.default;
+  }
+});
+Object.defineProperty(exports, "backendFallback", {
+  enumerable: true,
+  get: function () {
+    return _backend.default;
+  }
+});
+Object.defineProperty(exports, "close", {
+  enumerable: true,
+  get: function () {
+    return _close.default;
+  }
+});
+Object.defineProperty(exports, "codepen", {
+  enumerable: true,
+  get: function () {
+    return _codepen.default;
+  }
+});
+Object.defineProperty(exports, "codewars", {
+  enumerable: true,
+  get: function () {
+    return _codewars.default;
+  }
+});
+Object.defineProperty(exports, "command", {
+  enumerable: true,
+  get: function () {
+    return _command.default;
+  }
+});
+Object.defineProperty(exports, "commandFallback", {
+  enumerable: true,
+  get: function () {
+    return _command.default;
+  }
+});
+Object.defineProperty(exports, "competition", {
+  enumerable: true,
+  get: function () {
+    return _competition.default;
+  }
+});
+Object.defineProperty(exports, "competitionFallback", {
+  enumerable: true,
+  get: function () {
+    return _competition.default;
+  }
+});
+Object.defineProperty(exports, "css", {
+  enumerable: true,
+  get: function () {
+    return _css.default;
+  }
+});
+Object.defineProperty(exports, "cssFallback", {
+  enumerable: true,
+  get: function () {
+    return _css.default;
+  }
+});
+Object.defineProperty(exports, "devTools", {
+  enumerable: true,
+  get: function () {
+    return _devTools.default;
+  }
+});
+Object.defineProperty(exports, "devToolsFallback", {
+  enumerable: true,
+  get: function () {
+    return _devTools.default;
+  }
+});
+Object.defineProperty(exports, "docker", {
+  enumerable: true,
+  get: function () {
+    return _docker.default;
+  }
+});
+Object.defineProperty(exports, "dockerFallback", {
+  enumerable: true,
+  get: function () {
+    return _docker.default;
+  }
+});
+Object.defineProperty(exports, "figma", {
+  enumerable: true,
+  get: function () {
+    return _figma.default;
+  }
+});
+Object.defineProperty(exports, "figmaFallback", {
+  enumerable: true,
+  get: function () {
+    return _figma.default;
+  }
+});
+Object.defineProperty(exports, "framer", {
+  enumerable: true,
+  get: function () {
+    return _framer.default;
+  }
+});
+Object.defineProperty(exports, "framerFallback", {
+  enumerable: true,
+  get: function () {
+    return _framer.default;
+  }
+});
+Object.defineProperty(exports, "git", {
+  enumerable: true,
+  get: function () {
+    return _git.default;
+  }
+});
+Object.defineProperty(exports, "gitFallback", {
+  enumerable: true,
+  get: function () {
+    return _git.default;
+  }
+});
+Object.defineProperty(exports, "github", {
+  enumerable: true,
+  get: function () {
+    return _github.default;
+  }
+});
+Object.defineProperty(exports, "githubFallback", {
+  enumerable: true,
+  get: function () {
+    return _github.default;
+  }
+});
+Object.defineProperty(exports, "html", {
+  enumerable: true,
+  get: function () {
+    return _html.default;
+  }
+});
+Object.defineProperty(exports, "htmlFallback", {
+  enumerable: true,
+  get: function () {
+    return _html.default;
+  }
+});
+Object.defineProperty(exports, "hypervisor", {
+  enumerable: true,
+  get: function () {
+    return _hypervisor.default;
+  }
+});
+Object.defineProperty(exports, "hypervisorFallback", {
+  enumerable: true,
+  get: function () {
+    return _hypervisor.default;
+  }
+});
+Object.defineProperty(exports, "javascript", {
+  enumerable: true,
+  get: function () {
+    return _javascript.default;
+  }
+});
+Object.defineProperty(exports, "javascriptFallback", {
+  enumerable: true,
+  get: function () {
+    return _javascript.default;
+  }
+});
+Object.defineProperty(exports, "learn", {
+  enumerable: true,
+  get: function () {
+    return _learn.default;
+  }
+});
+Object.defineProperty(exports, "learnFallback", {
+  enumerable: true,
+  get: function () {
+    return _learn.default;
+  }
+});
+Object.defineProperty(exports, "linkedIn", {
+  enumerable: true,
+  get: function () {
+    return _linkedIn.default;
+  }
+});
+Object.defineProperty(exports, "linkedInFallback", {
+  enumerable: true,
+  get: function () {
+    return _linkedIn.default;
+  }
+});
+Object.defineProperty(exports, "linux", {
+  enumerable: true,
+  get: function () {
+    return _linux.default;
+  }
+});
+Object.defineProperty(exports, "linuxFallback", {
+  enumerable: true,
+  get: function () {
+    return _linux.default;
+  }
+});
+Object.defineProperty(exports, "logique", {
+  enumerable: true,
+  get: function () {
+    return _logique.default;
+  }
+});
+Object.defineProperty(exports, "logiqueFallback", {
+  enumerable: true,
+  get: function () {
+    return _logique.default;
+  }
+});
+Object.defineProperty(exports, "logo", {
+  enumerable: true,
+  get: function () {
+    return _cLogo.default;
+  }
+});
+Object.defineProperty(exports, "menu", {
+  enumerable: true,
+  get: function () {
+    return _menu.default;
+  }
+});
+Object.defineProperty(exports, "mobile", {
+  enumerable: true,
+  get: function () {
+    return _mobile.default;
+  }
+});
+Object.defineProperty(exports, "mobileFallback", {
+  enumerable: true,
+  get: function () {
+    return _mobile.default;
+  }
+});
+Object.defineProperty(exports, "mongodb", {
+  enumerable: true,
+  get: function () {
+    return _mongodb.default;
+  }
+});
+Object.defineProperty(exports, "mongodbFallback", {
+  enumerable: true,
+  get: function () {
+    return _mongodb.default;
+  }
+});
+Object.defineProperty(exports, "nodejs", {
+  enumerable: true,
+  get: function () {
+    return _nodejs.default;
+  }
+});
+Object.defineProperty(exports, "nodejsFallback", {
+  enumerable: true,
+  get: function () {
+    return _nodejs.default;
+  }
+});
+Object.defineProperty(exports, "os", {
+  enumerable: true,
+  get: function () {
+    return _OS.default;
+  }
+});
+Object.defineProperty(exports, "osFallback", {
+  enumerable: true,
+  get: function () {
+    return _OS.default;
+  }
+});
+Object.defineProperty(exports, "php", {
+  enumerable: true,
+  get: function () {
+    return _php.default;
+  }
+});
+Object.defineProperty(exports, "phpFallback", {
+  enumerable: true,
+  get: function () {
+    return _php.default;
+  }
+});
+Object.defineProperty(exports, "project1", {
+  enumerable: true,
+  get: function () {
+    return _portfolio.default;
+  }
+});
+Object.defineProperty(exports, "project1Fallback", {
+  enumerable: true,
+  get: function () {
+    return _portfolio.default;
+  }
+});
+Object.defineProperty(exports, "project2", {
+  enumerable: true,
+  get: function () {
+    return _jobit.default;
+  }
+});
+Object.defineProperty(exports, "project2Fallback", {
+  enumerable: true,
+  get: function () {
+    return _jobit.default;
+  }
+});
+Object.defineProperty(exports, "project3", {
+  enumerable: true,
+  get: function () {
+    return _tripguide.default;
+  }
+});
+Object.defineProperty(exports, "project3Fallback", {
+  enumerable: true,
+  get: function () {
+    return _tripguide.default;
+  }
+});
+Object.defineProperty(exports, "python", {
+  enumerable: true,
+  get: function () {
+    return _python.default;
+  }
+});
+Object.defineProperty(exports, "pythonFallback", {
+  enumerable: true,
+  get: function () {
+    return _python.default;
+  }
+});
+Object.defineProperty(exports, "reactjs", {
+  enumerable: true,
+  get: function () {
+    return _reactjs.default;
+  }
+});
+Object.defineProperty(exports, "reactjsFallback", {
+  enumerable: true,
+  get: function () {
+    return _reactjs.default;
+  }
+});
+Object.defineProperty(exports, "reddit", {
+  enumerable: true,
+  get: function () {
+    return _reddit.default;
+  }
+});
+Object.defineProperty(exports, "send", {
+  enumerable: true,
+  get: function () {
+    return _send.default;
+  }
+});
+Object.defineProperty(exports, "sql", {
+  enumerable: true,
+  get: function () {
+    return _sql.default;
+  }
+});
+Object.defineProperty(exports, "sqlFallback", {
+  enumerable: true,
+  get: function () {
+    return _sql.default;
+  }
+});
+Object.defineProperty(exports, "stackoverflow", {
+  enumerable: true,
+  get: function () {
+    return _stackoverflow.default;
+  }
+});
+Object.defineProperty(exports, "stackoverflowFallback", {
+  enumerable: true,
+  get: function () {
+    return _stackoverflow.default;
+  }
+});
+Object.defineProperty(exports, "tailwind", {
+  enumerable: true,
+  get: function () {
+    return _tailwind.default;
+  }
+});
+Object.defineProperty(exports, "tailwindFallback", {
+  enumerable: true,
+  get: function () {
+    return _tailwind.default;
+  }
+});
+Object.defineProperty(exports, "team", {
+  enumerable: true,
+  get: function () {
+    return _team.default;
+  }
+});
+Object.defineProperty(exports, "teamFallback", {
+  enumerable: true,
+  get: function () {
+    return _team.default;
+  }
+});
+Object.defineProperty(exports, "threejs", {
+  enumerable: true,
+  get: function () {
+    return _threejs.default;
+  }
+});
+Object.defineProperty(exports, "typescript", {
+  enumerable: true,
+  get: function () {
+    return _typescript.default;
+  }
+});
+Object.defineProperty(exports, "typescriptFallback", {
+  enumerable: true,
+  get: function () {
+    return _typescript.default;
+  }
+});
+Object.defineProperty(exports, "virtualbox", {
+  enumerable: true,
+  get: function () {
+    return _Virtualbox.default;
+  }
+});
+Object.defineProperty(exports, "virtualboxFallback", {
+  enumerable: true,
+  get: function () {
+    return _Virtualbox.default;
+  }
+});
+Object.defineProperty(exports, "vmware", {
+  enumerable: true,
+  get: function () {
+    return _vmware.default;
+  }
+});
+Object.defineProperty(exports, "vmwareFallback", {
+  enumerable: true,
+  get: function () {
+    return _vmware.default;
+  }
+});
+Object.defineProperty(exports, "web", {
+  enumerable: true,
+  get: function () {
+    return _web.default;
+  }
+});
+Object.defineProperty(exports, "webFallback", {
+  enumerable: true,
+  get: function () {
+    return _web2.default;
+  }
+});
+Object.defineProperty(exports, "windows", {
+  enumerable: true,
+  get: function () {
+    return _windows.default;
+  }
+});
+Object.defineProperty(exports, "windowsFallback", {
+  enumerable: true,
+  get: function () {
+    return _windows.default;
+  }
+});
+Object.defineProperty(exports, "world", {
+  enumerable: true,
+  get: function () {
+    return _world.default;
+  }
+});
+Object.defineProperty(exports, "worldFallback", {
+  enumerable: true,
+  get: function () {
+    return _world.default;
+  }
+});
+var _cLogo = _interopRequireDefault(require("./c-logo.avif"));
+var _backend = _interopRequireDefault(require("./backend.png"));
+var _mobile = _interopRequireDefault(require("./mobile.png"));
+var _web = _interopRequireDefault(require("./web.avif"));
+var _web2 = _interopRequireDefault(require("./web.png"));
+var _menu = _interopRequireDefault(require("./menu.svg"));
+var _close = _interopRequireDefault(require("./close.svg"));
+var _send = _interopRequireDefault(require("./send.svg"));
+var _command = _interopRequireDefault(require("./command.png"));
+var _github = _interopRequireDefault(require("./github.png"));
+var _linkedIn = _interopRequireDefault(require("./linkedIn.png"));
+var _codewars = _interopRequireDefault(require("./codewars.svg"));
+var _stackoverflow = _interopRequireDefault(require("./stackoverflow.png"));
+var _codepen = _interopRequireDefault(require("./codepen.webp"));
+var _reddit = _interopRequireDefault(require("./reddit.webp"));
+var _css = _interopRequireDefault(require("./tech/css.png"));
+var _docker = _interopRequireDefault(require("./tech/docker.png"));
+var _figma = _interopRequireDefault(require("./tech/figma.png"));
+var _git = _interopRequireDefault(require("./tech/git.png"));
+var _html = _interopRequireDefault(require("./tech/html.png"));
+var _javascript = _interopRequireDefault(require("./tech/javascript.png"));
+var _mongodb = _interopRequireDefault(require("./tech/mongodb.png"));
+var _nodejs = _interopRequireDefault(require("./tech/nodejs.png"));
+var _reactjs = _interopRequireDefault(require("./tech/reactjs.png"));
+var _tailwind = _interopRequireDefault(require("./tech/tailwind.png"));
+var _typescript = _interopRequireDefault(require("./tech/typescript.png"));
+var _threejs = _interopRequireDefault(require("./tech/threejs.svg"));
+var _sql = _interopRequireDefault(require("./tech/sql.png"));
+var _php = _interopRequireDefault(require("./tech/php.png"));
+var _python = _interopRequireDefault(require("./tech/python.png"));
+var _vmware = _interopRequireDefault(require("./tech/vmware.png"));
+var _Virtualbox = _interopRequireDefault(require("./tech/Virtualbox.png"));
+var _framer = _interopRequireDefault(require("./tech/framer.png"));
+var _portfolio = _interopRequireDefault(require("./portfolio.png"));
+var _jobit = _interopRequireDefault(require("./jobit.png"));
+var _tripguide = _interopRequireDefault(require("./tripguide.png"));
+var _anonym = _interopRequireDefault(require("./people/anonym.webp"));
+var _devTools = _interopRequireDefault(require("./devTools.png"));
+var _hypervisor = _interopRequireDefault(require("./hypervisor.png"));
+var _OS = _interopRequireDefault(require("./OS.png"));
+var _world = _interopRequireDefault(require("./world.png"));
+var _learn = _interopRequireDefault(require("./learn.png"));
+var _logique = _interopRequireDefault(require("./logique.png"));
+var _competition = _interopRequireDefault(require("./competition.png"));
+var _team = _interopRequireDefault(require("./team.png"));
+var _linux = _interopRequireDefault(require("./linux.png"));
+var _windows = _interopRequireDefault(require("./windows.png"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=index.js.map
